aacf482a0e13633a8f4ec2c9381532c11a22130f
antdbs <antoinedubois14@gmail.com>
2023-09-14 00:44:52 +0200

    add generic and singlar tests

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,17 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+with source as (
 
-from `dbt-tutorial`.jaffle_shop.orders
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+
+staged as (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from source
+)
+
+select * from staged
